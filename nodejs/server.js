24a56f564376fe8138ee90a0041bc6d81268f299
Dean Hope Robertson <deanhoperobertson@gmail.com>
2022-08-16 16:30:21 +0100

    creates nodejs server

---
+++ nodejs/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const hostname = "127.0.0.1"
+
+app.get('/', (req, res) => {
+  res.send('Hello, Its Docker Time!')
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
